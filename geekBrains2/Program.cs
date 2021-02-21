f3f83013fa0f57e6e19c3d9b43b1363ecf49dafd
Александр <sasha21l@mail.ru>
2021-02-22 01:29:19 +0500

    Все переделал

--- geekBrains2/Program.cs
+++ geekBrains2/Program.cs
@@ -8,82 +8,255 @@ namespace geekBrain2_6
 {
     class Program
     {
-        public static int grafN = 5;
+        public static Point[] points = new Point[10];
+        public static Rebro[] rebra = new Rebro[9];
+        public static Point BeginPoint = new Point();
+
+
+
         static void Main(string[] args)
         {
-            Graf[,] mas = new Graf[grafN, grafN];
+            int name = 1;
             Random rnd = new Random();
-            int saveRand = -10;
-            int randNums = -10;
-            for (int i = 0; i < grafN; i++)
+            points[0] = new Point(9999, false, "A");
+            points[1] = new Point(9999, false, "B");
+            points[1].predPoint = points[0];
+            points[2] = new Point(9999, false, "C");
+            points[2].predPoint = points[1];
+            points[3] = new Point(9999, false, "D");
+            points[3].predPoint = points[2];
+            points[4] = new Point(9999, false, "E");
+            points[4].predPoint = points[2];
+            points[5] = new Point(9999, false, "D2");
+            points[5].predPoint = points[4];
+            points[6] = new Point(9999, false, "C2");
+            points[6].predPoint = points[0];
+            points[7] = new Point(9999, false, "E2");
+            points[7].predPoint = points[0];
+            points[8] = new Point(9999, false, "C3");
+            points[8].predPoint = points[7];
+            points[9] = new Point(9999, false, "D3");
+            points[9].predPoint = points[7];
+
+            rebra[0] = new Rebro(points[0], points[1], 2);
+            rebra[1] = new Rebro(points[1], points[2], 1);
+            rebra[2] = new Rebro(points[2], points[3], 5);
+            rebra[3] = new Rebro(points[2], points[4], 1);
+            rebra[4] = new Rebro(points[4], points[5], 3);
+            rebra[5] = new Rebro(points[0], points[6], 5);
+            rebra[6] = new Rebro(points[0], points[7], 7);
+            rebra[7] = new Rebro(points[7], points[8], 1);
+            rebra[8] = new Rebro(points[7], points[9], 3);
+
+            BeginPoint = points[0];
+
+            DekstraAlgorim Deks = new DekstraAlgorim(points, rebra);
+            Deks.AlgoritmRun(BeginPoint);
+            Deks.OneStep(points[0]);
+            Deks.MinPath1(points[3]);
+
+            List<String> myList = PrintGrath.PrintAllPoints(Deks);
+            List<String> myList2 = PrintGrath.PrintAllMinPaths(Deks);
+
+            for (int i = 0; i < myList.Count; i++)
             {
-                for (int j = 0; j < grafN; j++)
-                {
-                    while (randNums == saveRand)
-                    {
-                        randNums = rnd.Next(1, 10);
-                    }
-                    saveRand = randNums;
-                    if (randNums > 10)
-                    {
-                        mas[i, j] = new Graf();
-                        mas[i, j].weight = 0;
-                        if (mas[i, j].grafNumber == -100) mas[i, j].grafNumber = i + 1;
-                        if (mas[i, j].intersectionWithGraph == -100) mas[i, j].intersectionWithGraph = j + 1;
-                        Console.Write("(" + mas[i, j].grafNumber + "-" + mas[i, j].intersectionWithGraph + ") " + mas[i, j].weight + "\t"); //(Отношение графа друг к другу) Вес этого отдельного графа"
-                        continue;
-                    }
-                    mas[i, j] = new Graf();
-                    mas[i, j].weight = rnd.Next(1, 10);
-                    if (mas[i, j].grafNumber == -100) mas[i, j].grafNumber = i + 1;
-                    if (mas[i, j].intersectionWithGraph == -100) mas[i, j].intersectionWithGraph = j + 1;
-                    Console.Write("(" + mas[i, j].grafNumber + "-" + mas[i, j].intersectionWithGraph + ") " + mas[i, j].weight + "\t"); //(Отношение графа друг к другу) Вес этого отдельного графа"
-                }
-                Console.WriteLine();
+                Console.WriteLine(myList[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < myList2.Count; i++)
+            {
+                Console.WriteLine(myList2[i]);
             }
 
-            test(mas, mas[0, 0]);
             Console.ReadLine();
         }
-        public static void test(Graf[,] mas, Graf grafGeneral)
+    }
+            
+    class DekstraException : ApplicationException
+    {
+        public DekstraException(string message) : base(message)
         {
-            int sumWeight;
-            Graf grafMinWeight;
-            int sumWeightMin = 1000;
-            int indexGraf = 0;
-
-            foreach (Graf gr in mas)
-            {
-                if (gr == grafGeneral) break;
-                indexGraf++;
-            }
-            for (int i = 0; i < grafN; i++)
-            {
-                try
-                {
-                    if (mas[indexGraf, i] == null || mas[indexGraf, i] == grafGeneral || mas[indexGraf, i].attended == true) continue;
-                }
-                catch
-                {
-                    break;
-                }
-                sumWeight = grafGeneral.weight + mas[indexGraf, i].weight;
-                mas[indexGraf, i].sumWeight = sumWeight;
-                mas[indexGraf, i].attended = true;
-                Console.WriteLine("");
-                Console.Write("(" + mas[indexGraf, i].grafNumber + "-" + mas[indexGraf, i].intersectionWithGraph + ") " + mas[indexGraf, i].sumWeight + "- общий вес до графа" + "\t"); // (Отношение графа друг к другу)Вес всех графов до этого"
-                test(mas, mas[indexGraf, i]);
-            }
-
 
         }
-        public class Graf
+    }
+    static class PrintGrath
+    {
+        public static List<string> PrintAllPoints(DekstraAlgorim da)
         {
-            public int grafNumber = -100;
-            public int intersectionWithGraph = -100;
-            public int sumWeight;
-            public bool attended = false;
-            public int weight;
+            List<string> retListOfPoints = new List<string>();
+            foreach (Point p in da.points)
+            {
+                retListOfPoints.Add(string.Format("point name={0}, point value={1}, predok={2}", p.Name, p.ValueMetka, p.predPoint.Name ?? "нет предка"));
+            }
+            return retListOfPoints;
+        }
+        public static List<string> PrintAllMinPaths(DekstraAlgorim da)
+        {
+            List<string> retListOfPointsAndPaths = new List<string>();
+            foreach (Point p in da.points)
+            {
+
+                if (p != da.BeginPoint)
+                {
+                    string s = string.Empty;
+                    foreach (Point p1 in da.MinPath1(p))
+                    {
+                        s += string.Format("{0} ", p1.Name);
+                    }
+                    retListOfPointsAndPaths.Add(string.Format("Point ={0},MinPath from {1} = {2}", p.Name, da.BeginPoint.Name, s));
+                }
+
+            }
+            return retListOfPointsAndPaths;
+
+        }
+    }
+
+    class Point
+    {
+        public float ValueMetka { get; set; }
+        public string Name { get; private set; }
+        public bool IsChecked { get; set; }
+        public Point predPoint { get; set; }
+        public object SomeObj { get; set; }
+        public Point(int value, bool ischecked)
+        {
+            ValueMetka = value;
+            IsChecked = ischecked;
+            predPoint = new Point();
+        }
+        public Point(int value, bool ischecked, string name)
+        {
+            ValueMetka = value;
+            IsChecked = ischecked;
+            Name = name;
+            predPoint = new Point();
+        }
+        public Point()
+        {
+        }
+    }
+    class Rebro
+    {
+        public Point FirstPoint { get; private set; }
+        public Point SecondPoint { get; private set; }
+        public float Weight { get; private set; }
+
+        public Rebro(Point first, Point second, float valueOfWeight)
+        {
+            FirstPoint = first;
+            SecondPoint = second;
+            Weight = valueOfWeight;
+        }
+    }
+    class DekstraAlgorim
+    {
+
+        public Point[] points { get; private set; }
+        public Rebro[] rebra { get; private set; }
+        public Point BeginPoint { get; private set; }
+
+        public DekstraAlgorim(Point[] pointsOfgrath, Rebro[] rebraOfgrath)
+        {
+            points = pointsOfgrath;
+            rebra = rebraOfgrath;
+        }
+        public void AlgoritmRun(Point beginp)
+        {
+            if (this.points.Count() == 0 || this.rebra.Count() == 0)
+            {
+                throw new DekstraException("Массив вершин или ребер не задан!");
+            }
+            else
+            {
+                BeginPoint = beginp;
+                OneStep(beginp);
+                foreach (Point point in points)
+                {
+                    Point anotherP = GetAnotherUncheckedPoint();
+                    if (anotherP != null)
+                    {
+                        OneStep(anotherP);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        public void OneStep(Point beginpoint)
+        {
+            foreach (Point nextp in Pred(beginpoint))
+            {
+                if (nextp.IsChecked == false)//не отмечена
+                {
+                    float newmetka = beginpoint.ValueMetka + GetMyRebro(nextp, beginpoint).Weight;
+                    if (nextp.ValueMetka > newmetka)
+                    {
+                        nextp.ValueMetka = newmetka;
+                        nextp.predPoint = beginpoint;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            beginpoint.IsChecked = true;//вычеркиваем
+        }
+        private IEnumerable<Point> Pred(Point currpoint)
+        {
+            IEnumerable<Point> firstpoints = from ff in rebra where ff.FirstPoint == currpoint select ff.SecondPoint;
+            IEnumerable<Point> secondpoints = from sp in rebra where sp.SecondPoint == currpoint select sp.FirstPoint;
+            IEnumerable<Point> totalpoints = firstpoints.Concat<Point>(secondpoints);
+            return totalpoints;
+        }
+        private Rebro GetMyRebro(Point a, Point b)
+        {//ищем ребро по 2 точкам
+            IEnumerable<Rebro> myr = from reb in rebra where (reb.FirstPoint == a & reb.SecondPoint == b) || (reb.SecondPoint == a & reb.FirstPoint == b) select reb;
+            if (myr.Count() > 1 || myr.Count() == 0)
+            {
+                throw new DekstraException("Не найдено ребро между соседями!");
+            }
+            else
+            {
+                return myr.First();
+            }
+        }
+        private Point GetAnotherUncheckedPoint()
+        {
+            IEnumerable<Point> pointsuncheck = from p in points where p.IsChecked == false select p;
+            if (pointsuncheck.Count() != 0)
+            {
+                float minVal = pointsuncheck.First().ValueMetka;
+                Point minPoint = pointsuncheck.First();
+                foreach (Point p in pointsuncheck)
+                {
+                    if (p.ValueMetka < minVal)
+                    {
+                        minVal = p.ValueMetka;
+                        minPoint = p;
+                    }
+                }
+                return minPoint;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public List<Point> MinPath1(Point end)
+        {
+            List<Point> listOfpoints = new List<Point>();
+            Point tempp = new Point();
+            tempp = end;
+            while (tempp != this.BeginPoint)
+            {
+                listOfpoints.Add(tempp);
+                tempp = tempp.predPoint;
+            }
+            return listOfpoints;
         }
     }
 }
